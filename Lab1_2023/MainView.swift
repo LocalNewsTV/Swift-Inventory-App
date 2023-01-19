76d2e8c736318b191f50eff1b57e3753ea0719ef
Matthew Logan <loganmatthewdev@gmail.com>
2023-01-18 21:57:55 -0800

    Added more Window Sizes

--- Lab1_2023/MainView.swift
+++ Lab1_2023/MainView.swift
@@ -11,13 +11,20 @@ struct MainView: View {
     @State private var showSettings = false
     @State var colour = array2color(array: UserDefaults.standard.object(forKey: "BackgroundColour") as? [CGFloat] ?? color2array(colour: Color.yellow))
     @State var charLimit = UserDefaults.standard.object(forKey: "MaxCharacterCount") as? Int ?? 150
+    @Environment(\.horizontalSizeClass) var sizeClass
     var body: some View {
         NavigationStack() {
             VStack {
                 if showSettings {
                     SettingsView(colour: $colour, charLimit: $charLimit)
                 } else {
-                    DetailView(colour: colour, charLimit: charLimit)
+                    if sizeClass == .regular {
+                        DetailView(colour: colour, charLimit: charLimit)
+                            .frame(width: 320, height: 460, alignment: .center)
+                    }
+                    else if sizeClass == .compact {
+                        DetailView(colour: colour, charLimit: charLimit)
+                    }
                 }
             }
             .navigationBarItems(
@@ -34,6 +41,11 @@ struct MainView: View {
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        MainView()
+//        MainView()
+        ForEach(["iPad (10th generation)", "iPhone 14 Pro"], id: \.self) {
+            deviceName in
+            MainView()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+        }
     }
 }
