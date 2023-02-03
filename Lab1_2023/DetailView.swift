dfc321c7cc236fe7ff21fe7bc19028fccb14da17
Matthew Logan <ics26-021@camosun.bc.ca>
2023-02-03 14:22:08 -0800

    Added Camera Button/Settings

--- Lab1_2023/DetailView.swift
+++ Lab1_2023/DetailView.swift
@@ -6,13 +6,16 @@
 //
 
 import SwiftUI
-
+import Photos
 struct DetailView: View {
     var colour: Color
     var charLimit: Int
+    @State var pickerVisible = false
+    @State var showCameraAlert = false
+    @State var imageSource = UIImagePickerController.SourceType.camera
     @Binding var inventoryItem: InventoryItem
     var body: some View {
-//        let defaultColor = Color.white
+        //        let defaultColor = Color.white
         VStack {
             Image(systemName: inventoryItem.image)
                 .resizable(resizingMode: .stretch)
@@ -34,16 +37,34 @@ struct DetailView: View {
                         inventoryItem.description = newValue
                     }
                 }
-                )
+            )
             )
             .accessibilityIdentifier("DetailTextEditor")
             Text(String("\(inventoryItem.description.count)/\(charLimit)"))
-            .accessibilityIdentifier("DetailText")
+                .accessibilityIdentifier("DetailText")
         }
         .padding()
+        .navigationBarItems(trailing:
+                                Button(action: {
+            AVCaptureDevice.requestAccess(for: AVMediaType.video){ response in
+                if response && UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera){
+                    self.showCameraAlert = false
+                    self.imageSource = UIImagePickerController.SourceType.camera
+                    self.pickerVisible.toggle()
+                } else {
+                    self.showCameraAlert = true
+                }
+            }
+        }) {
+            Image(systemName: "camera")
+        }
+            .alert(isPresented: $showCameraAlert) {
+                Alert(title: Text("Error"), message: Text("Camera not available"), dismissButton: .default(Text("OK")))
+                
+            }
+        )
     }
 }
-
 struct DetailView_Previews: PreviewProvider {
     @State var inventoryItems: InventoryItems
     static let defaultColor = Color.yellow
